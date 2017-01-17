d20811ca151446d347ea7f732c43d083a2d200c8
ASA <asa.kwarnmark@gmail.com>
2017-01-17 13:52:42 +0100

    Initial Commit

---
+++ Labb2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb2
+//
+//  Created by Åsa Kwarnmark on 2017-01-17.
+//  Copyright © 2017 Åsa Kwarnmark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Labb2
+//
+//  Created by Åsa Kwarnmark on 2017-01-17.
+//  Copyright © 2017 Åsa Kwarnmark. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
